213e7bb2bdc9b8ade7f5242f56e0d0f9a0fac66f
Mian Hamza <MuhammadHamzaShahid@github.com>
2022-04-28 04:36:12 +0500

    fresh suitecrm

---
+++ custom/Extension/modules/Meetings/Ext/Vardefs/_override_sugarfield_jjwg_maps_geocode_status_c.php
@@ -0,0 +1,5 @@
+<?php
+ // created: 2022-04-28 01:35:12
+$dictionary['Meeting']['fields']['jjwg_maps_geocode_status_c']['inline_edit']=1;
+
+ ?>
---
+++ custom/Extension/modules/Prospects/Ext/Vardefs/_override_sugarfield_jjwg_maps_lng_c.php
@@ -0,0 +1,5 @@
+<?php
+ // created: 2022-04-28 01:35:12
+$dictionary['Prospect']['fields']['jjwg_maps_lng_c']['inline_edit']=1;
+
+ ?>
